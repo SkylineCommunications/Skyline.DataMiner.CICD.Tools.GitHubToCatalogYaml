9d8a9cdb8f37b579d5d934757ac3f5800e4b18a7
Jan Staelens <jan.staelens@skyline.be>
2025-01-06 12:57:02 +0100

    QuickFix: Re-Add support for lowercase and old types to map correctly to the new types.

--- Skyline.DataMiner.CICD.Tools.GitHubToCatalogYaml/Constants.cs
+++ Skyline.DataMiner.CICD.Tools.GitHubToCatalogYaml/Constants.cs
@@ -8,24 +8,24 @@
     {
         public static readonly List<ArtifactType> ArtifactTypeMap = new List<ArtifactType>()
         {
-            { new ArtifactType(new[] { "AS" }, "Automation", "Automation Script") },
-            { new ArtifactType(new[] { "C" }, "Connector", "Connector")},
-            { new ArtifactType(new[] { "CF" }, "Custom Solution", "Companion File")},
-            { new ArtifactType(new[] { "CHATOPS" }, "ChatOps Extension", "ChatOps Extension")},
-            { new ArtifactType(new[] { "D" }, "Dashboard", "Dashboard")},
-            { new ArtifactType(new[] { "DOC" }, "Custom Solution", "Documentation")},
-            { new ArtifactType(new[] { "F" }, "Custom Solution", "Function Definition")},
-            { new ArtifactType(new[] { "GQIDS" }, "Ad Hoc Data Source", "gqidatasource", "Ad Hoc Data Source")},
-            { new ArtifactType(new[] { "GQIO" }, "Data Transformer", "GQI Operator")},
-            { new ArtifactType(new[] { "LSO" }, "Automation", "Live Cycle Service Orchestration")},
-            { new ArtifactType(new[] { "PA" }, "Automation", "Process Automation") },
-            { new ArtifactType(new[] { "PLS" }, "Automation", "Profile Load Script") },
-            { new ArtifactType(new[] { "S" }, "Custom Solution", "Solution") },
-            { new ArtifactType(new[] { "SC" }, "Scripted Connector", "Scripted Connector") },
-            { new ArtifactType(new[] { "T" }, "Custom Solution", "Testing Solution") },
-            { new ArtifactType(new[] { "UDAPI" }, "User-Defined API", "User Defined API") },
-            { new ArtifactType(new[] { "V" }, "Visual Overview", "Visio") },
-            { new ArtifactType(new[] { "LCA" }, "Custom Solution", "Low-Code App") }
+            { new ArtifactType(new[] { "AS" }, "Automation", "Automation Script", "automationscript") },
+            { new ArtifactType(new[] { "C" }, "Connector", "Connector", "connector")},
+            { new ArtifactType(new[] { "CF" }, "Custom Solution", "Companion File", "companionfile")},
+            { new ArtifactType(new[] { "CHATOPS" }, "ChatOps Extension", "ChatOps Extension", "chatopsextension")},
+            { new ArtifactType(new[] { "D" }, "Dashboard", "Dashboard", "dashboard")},
+            { new ArtifactType(new[] { "DOC" }, "Custom Solution", "Documentation", "documentation")},
+            { new ArtifactType(new[] { "F" }, "Custom Solution", "Function Definition", "functiondefinition")},
+            { new ArtifactType(new[] { "GQIDS" }, "Ad Hoc Data Source", "gqidatasource", "Ad Hoc Data Source", "adhocdatasource")},
+            { new ArtifactType(new[] { "GQIO" }, "Data Transformer", "GQI Operator", "gqioperator")},
+            { new ArtifactType(new[] { "LSO" }, "Automation", "Live Cycle Service Orchestration", "lifecycleserviceorchestration")},
+            { new ArtifactType(new[] { "PA" }, "Automation", "Process Automation", "processautomation") },
+            { new ArtifactType(new[] { "PLS" }, "Automation", "Profile Load Script", "profileloadscript") },
+            { new ArtifactType(new[] { "S" }, "Custom Solution", "Solution", "solution") },
+            { new ArtifactType(new[] { "SC" }, "Scripted Connector", "Scripted Connector", "scriptedconnector") },
+            { new ArtifactType(new[] { "T" }, "Custom Solution", "Testing Solution", "testingsolution") },
+            { new ArtifactType(new[] { "UDAPI" }, "User-Defined API", "User Defined API", "userdefinedapi") },
+            { new ArtifactType(new[] { "V" }, "Visual Overview", "Visio", "visio") },
+            { new ArtifactType(new[] { "LCA" }, "Custom Solution", "Low-Code App", "lowcodeapp") }
         };
     }
 
