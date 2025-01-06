2c808b865ead617f8f8704a623b4be9d149d760e
Jan Staelens <jan.staelens@skyline.be>
2025-01-06 17:22:28 +0100

    Added topics from github guidelines SLC.

--- Skyline.DataMiner.CICD.Tools.GitHubToCatalogYaml/Constants.cs
+++ Skyline.DataMiner.CICD.Tools.GitHubToCatalogYaml/Constants.cs
@@ -8,35 +8,29 @@
     {
         public static readonly List<ArtifactType> ArtifactTypeMap = new List<ArtifactType>()
         {
-            { new ArtifactType(new[] { "AS" }, "Automation", "Automation Script", "automationscript") },
-            { new ArtifactType(new[] { "C" }, "Connector", "Connector", "connector")},
-            { new ArtifactType(new[] { "CF" }, "Custom Solution", "Companion File", "companionfile")},
-            { new ArtifactType(new[] { "CHATOPS" }, "ChatOps Extension", "ChatOps Extension", "chatopsextension")},
-            { new ArtifactType(new[] { "D" }, "Dashboard", "Dashboard", "dashboard")},
-            { new ArtifactType(new[] { "DOC" }, "Custom Solution", "Documentation", "documentation")},
-            { new ArtifactType(new[] { "F" }, "Custom Solution", "Function Definition", "functiondefinition")},
-            { new ArtifactType(new[] { "GQIDS" }, "Ad Hoc Data Source", "gqidatasource", "Ad Hoc Data Source", "adhocdatasource")},
-            { new ArtifactType(new[] { "GQIO" }, "Data Transformer", "GQI Operator", "gqioperator")},
-            { new ArtifactType(new[] { "LSO" }, "Automation", "Live Cycle Service Orchestration", "lifecycleserviceorchestration")},
-            { new ArtifactType(new[] { "PA" }, "Automation", "Process Automation", "processautomation") },
-            { new ArtifactType(new[] { "PLS" }, "Automation", "Profile Load Script", "profileloadscript") },
-            { new ArtifactType(new[] { "S" }, "Custom Solution", "Solution", "solution") },
-            { new ArtifactType(new[] { "SC" }, "Scripted Connector", "Scripted Connector", "scriptedconnector") },
-            { new ArtifactType(new[] { "T" }, "Custom Solution", "Testing Solution", "testingsolution") },
-            { new ArtifactType(new[] { "UDAPI" }, "User-Defined API", "User Defined API", "userdefinedapi") },
-            { new ArtifactType(new[] { "V" }, "Visual Overview", "Visio", "visio") },
-            { new ArtifactType(new[] { "LCA" }, "Custom Solution", "Low-Code App", "lowcodeapp") }
+            { new ArtifactType(new[] { "AS" }, "Automation", "Automation Script", "automationscript", "dataminer-automation-script") },
+            { new ArtifactType(new[] { "C" }, "Connector", "Connector", "connector","dataminer-connector")},
+            { new ArtifactType(new[] { "CF" }, "Custom Solution", "Companion File", "companionfile","dataminer-companion-file")},
+            { new ArtifactType(new[] { "CHATOPS" }, "ChatOps Extension", "ChatOps Extension", "chatopsextension", "dataminer-bot", "dataminer-chatops")},
+            { new ArtifactType(new[] { "D" }, "Dashboard", "Dashboard", "dashboard", "dataminer-dashboard")},
+            { new ArtifactType(new[] { "DOC" }, "Custom Solution", "Documentation", "documentation", "dataminer-doc")},
+            { new ArtifactType(new[] { "F" }, "Custom Solution", "Function Definition", "functiondefinition", "dataminer-function")},
+            { new ArtifactType(new[] { "GQIDS" }, "Ad Hoc Data Source", "gqidatasource", "Ad Hoc Data Source", "adhocdatasource", "dataminer-gqi-data-source")},
+            { new ArtifactType(new[] { "GQIO" }, "Data Transformer", "GQI Operator", "gqioperator", "dataminer-gqi-operator")},
+            { new ArtifactType(new[] { "LSO" }, "Automation", "Live Cycle Service Orchestration", "lifecycleserviceorchestration", "dataminer-life-service-orchestration")},
+            { new ArtifactType(new[] { "PA" }, "Automation", "Process Automation", "processautomation", "dataminer-process-automation-script") },
+            { new ArtifactType(new[] { "PLS" }, "Automation", "Profile Load Script", "profileloadscript", "dataminer-profile-load-script") },
+            { new ArtifactType(new[] { "S" }, "Custom Solution", "Solution", "solution", "dataminer","dataminer-solution", "dataminer-dis-macro", "dataminer-nuget") },
+            { new ArtifactType(new[] { "SC" }, "Scripted Connector", "Scripted Connector", "scriptedconnector","dataminer-scripted-connector") },
+            { new ArtifactType(new[] { "T" }, "Custom Solution", "Testing Solution", "testingsolution", "dataminer-regression-test", "dataminer-UI-test") },
+            { new ArtifactType(new[] { "UDAPI" }, "User-Defined API", "User Defined API", "userdefinedapi", "dataminer-user-defined-api") },
+            { new ArtifactType(new[] { "V" }, "Visual Overview", "Visio", "visio","dataminer-visio") },
+            { new ArtifactType(new[] { "LCA" }, "Custom Solution", "Low-Code App", "lowcodeapp", "dataminer-low-code-app") }
         };
     }
 
     internal class ArtifactType
     {
-        public string[] GitHubNames { get; set; }
-
-        public string[] GitHubAbbreviations { get; set; }
-
-        public string CatalogName { get; set; }
-
         public ArtifactType(string[] abbreviations, string catalogName, params string[] githubNames)
         {
             GitHubAbbreviations = abbreviations;
@@ -45,6 +39,12 @@
 
             CatalogName = catalogName;
         }
+
+        public string CatalogName { get; set; }
+
+        public string[] GitHubAbbreviations { get; set; }
+
+        public string[] GitHubNames { get; set; }
 
         public bool IsMatch(string searchTerm)
         {
@@ -58,5 +58,4 @@
             return false;
         }
     }
-
 }
